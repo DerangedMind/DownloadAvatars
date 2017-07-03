1ae1e44ac3905453dd50665cc88786667e84f70e
Riki <rick.libby.91@gmail.com>
2017-07-03 19:42:36 +0000

    parse response as JSON and cycle through avatar_url keys of contributors

--- download_avatars.js
+++ download_avatars.js
@@ -14,8 +14,9 @@
 // token: fd6bd42b0966cd0392bfc92bc9a2daac12b38339
 
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "DerangedMind";
-var GITHUB_TOKEN = "b4a3384177522805bb14eb9a764155bd034f4e37";
+var GITHUB_TOKEN = "80fa3987b1f0846b40c281ace9801490d4937bf8";
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
@@ -25,15 +26,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
     headers: {
       'User-Agent': 'DerangedMind'
     }
-  }
-  console.log(requestURL);
-  request(requestURL, cb);
+  };
+
+  request(requestURL, cb)
   
 }
 
 getRepoContributors("jquery", "jquery", function (err, result, body) {
   console.log("Errors: " + err);
-  console.log("Result: " + result.body);
+  var bodyJSON = JSON.parse(body);
+  for (var contributor in bodyJSON) {
+    console.log(bodyJSON[contributor]['avatar_url'])
+  }
+
 
 
 })

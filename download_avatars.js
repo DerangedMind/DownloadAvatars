a7459c73cdd700852b4c4afdd9603c17fd168c33
Riki <rick.libby.91@gmail.com>
2017-07-03 15:35:43 +0000

    add user, token, and url request strings

--- download_avatars.js
+++ download_avatars.js
@@ -11,12 +11,17 @@
 
 // login, avatar_url
 
+// token: fd6bd42b0966cd0392bfc92bc9a2daac12b38339
+
 var request = require('request');
+var GITHUB_USER = "DerangedMind";
+var GITHUB_TOKEN = "fd6bd42b0966cd0392bfc92bc9a2daac12b38339";
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {

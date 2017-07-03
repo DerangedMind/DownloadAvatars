f494adc933177923c5c57ae82e7896c9a54cb4f3
Riki <rick.libby.91@gmail.com>
2017-07-03 14:57:51 +0000

    start project

---
+++ download_avatars.js
@@ -0,0 +1,8 @@
+// var request = require('request');
+// use git for version control
+
+// call script via terminal with args:
+//   node download_avatars.js <GH_USER> <REPO>
+
+// download images to folder "./avatars/" 
+// images should be named <GH_USER>.png

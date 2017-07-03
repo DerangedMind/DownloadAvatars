ff7bf54a21357fd34283cc922ab248f9e301c122
Riki <rick.libby.91@gmail.com>
2017-07-03 19:07:10 +0000

    new key and send User-Agent in header

--- download_avatars.js
+++ download_avatars.js
@@ -15,16 +15,26 @@
 
 var request = require('request');
 var GITHUB_USER = "DerangedMind";
-var GITHUB_TOKEN = "fd6bd42b0966cd0392bfc92bc9a2daac12b38339";
+var GITHUB_TOKEN = "b4a3384177522805bb14eb9a764155bd034f4e37";
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'DerangedMind'
+    }
+  }
   console.log(requestURL);
+  request(requestURL, cb);
+  
 }
 
-getRepoContributors("jquery", "jquery", function (err, result) {
+getRepoContributors("jquery", "jquery", function (err, result, body) {
   console.log("Errors: " + err);
-  console.log("Result: " + result);
+  console.log("Result: " + result.body);
+
+
 })
+

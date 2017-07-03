f893f064aed273506f5d3ed9a629d213a1f29306
Riki <rick.libby.91@gmail.com>
2017-07-03 15:29:53 +0000

    add getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,3 @@
-// var request = require('request');
 // use git for version control
 
 // call script via terminal with args:
@@ -11,3 +10,16 @@
 // GitHub API JSON keys:
 
 // login, avatar_url
+
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  
+}
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+  console.log("Errors: " + err);
+  console.log("Result: " + result);
+})

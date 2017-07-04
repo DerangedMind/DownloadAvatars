e4dfc295dba6f8418da63887893858ecef1d93e7
Riki <rick.libby.91@gmail.com>
2017-07-04 14:19:41 +0000

    all pics work now because of file types, but ut it's not adding endings

--- download_avatars.js
+++ download_avatars.js
@@ -19,7 +19,9 @@ require('dotenv').config();
 
 var GITHUB_USER = process.env.GITHUB_USER;
 var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-console.log(GITHUB_USER)
+var SAVE_TO_FOLDER = './avatars/'
+
+var GITHUB_URL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/`;
 
 var stdARGS = process.argv.slice(2);
 
@@ -32,59 +34,103 @@ function checkArgs() {
     console.log("You forgot to include an owner or repo!");
     return;
   }
-  console.log("what");
-  getRepoContributors(stdARGS[0], stdARGS[1], handleRequest);
+  if (stdARGS.length > 2) {
+    console.log("You included too many arguments!");
+    return;
+  }
+  console.log("User exists? " + userExists());
+
+  getRepoContributors(handleRequest);
+  
 }
 
-function getRepoContributors(repoOwner, repoName, callback) {
+function checkExistance() {
+
+}
+
+// to check if user exists, 
+//  go to api.github.com/users/<repoOwner>
+//  parse it as a json
+//  check if the key "message" is Not Found
+//    if yes, return false and stop running
+//    if no, continue
+
+function userExists() {
   var requestURL = {
-    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    url: GITHUB_URL + 'users/' + stdARGS[0],
+    headers: {
+      'User-Agent': 'DerangedMind'
+    }
+  }
+
+  request
+    .get(requestURL)
+    .on('response', function (response) {
+      console.log(response)
+    }) 
+  // (requestURL, function(err, result, body) {
+  //   if (err) { throw console.log("Errors: " + err); }
+
+  //   var json = parseJSON(body);
+  //   for (var gitUser in json) {
+  //     console.log("hello?")
+  //     if (json[gitUser]["messages"] === "Not Found") {
+  //       return false;
+  //     }
+  //   }
+  // });
+}
+
+function getRepoContributors(callback) {
+  var requestURL = {
+    url: GITHUB_URL + 'repos/' + stdARGS[0] + '/' + stdARGS[1] + '/contributors',
     headers: {
       'User-Agent': 'DerangedMind'
     }
   };
-  console.log("where do we get stuck?");
+
   request(requestURL, callback);
-  
 }
 
 function handleRequest(err, result, body) {
-  if (err) {
-    throw console.log("Errors: " + err);
-  }
+  if (err) { throw console.log("Errors: " + err); }
+  var json = parseJSON(body);
 
-  cycleContributors(result, parseJSON(body));
-  
+  cycleContributors(result, json);
+}
+
+function parseJSON(body) {
+  return JSON.parse(body);
+}
+
+function cycleContributors(result, _bodyJSON) {
+  fs.access(SAVE_TO_FOLDER, function (err) {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdir(SAVE_TO_FOLDER);
+    }
+  });
+    
+  for (var contributor in _bodyJSON) {
+    downloadImageByURL(result, _bodyJSON[contributor]);
+  } 
 }
 
 function downloadImageByURL(result, contributor) {
   var username = contributor['login'];
   var _url = contributor['avatar_url'];
-  
+  var contentType = '';
+
 
   request
     .get(_url)
-    .on('response', function() {
+    .on('response', function(response) {
       console.log("Downloading avatars...");
+      contentType = response.headers['content-type'].split('/')[1];
+      console.log(contentType);
+      console.log(`${SAVE_TO_FOLDER}${username}.${contentType}`);
     })
-    .pipe(fs.createWriteStream('./avatars/' + username + '.jpg'))
+    .pipe(fs.createWriteStream(`${SAVE_TO_FOLDER}${username}.${contentType}`))
     .on('finish', function() {
       console.log("Download complete.");
     });
 }
-
-
-
-function parseJSON(body) {
-
-  return JSON.parse(body);
-}
-
-function cycleContributors(result, _bodyJSON) {
-
-  for (var contributor in _bodyJSON) {
-    downloadImageByURL(result, _bodyJSON[contributor]);
-  }
-  
-}
-
